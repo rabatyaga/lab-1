1c77c763b07e640b798a07421695c445559f8f30
rabatyaga <36544216+rabatyaga@users.noreply.github.com>
2018-03-29 15:44:06 +0300

    Create fibbonaci.java

---
+++ lab - 3/fibbonaci.java
@@ -0,0 +1,27 @@
+package com.tasks3.fibonacci;
+
+public class Fibonacci
+{
+    public static long getNumber(int position)
+    {
+        if (position <= 0)
+        {
+            return -1;
+        }
+
+        if (position == 1 || position == 2)
+        {
+            return 1;
+        }
+
+        long[] nums = new long[position];
+        nums[0] = nums[1] = 1;
+
+        for (int i = 2; i < position; i++)
+        {
+            nums[i] = nums[i - 1] + nums[i - 2];
+        }
+
+        return nums[position - 1];
+    }
+}
